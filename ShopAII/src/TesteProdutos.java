6944d01e7596bb9a79ce834005cce18bea9a5cbd
Israel Antunes Neto <israneto20@gmail.com>
2023-10-25 16:02:16 -0300

    Versão 0.2

--- ShopAII/src/TesteProdutos.java
+++ ShopAII/src/TesteProdutos.java
@@ -1,14 +1,98 @@
 
+//Deu bom
+import java.util.Scanner;
+
 public class TesteProdutos {
     public static void main(String[] args) {
-       
-        Eletronico eletronico = new Eletronico("TV LED", 799.99, "Smart TV 55 polegadas", 10, "Samsung", "Modelo ABC123");
-        Vestuario vestuario = new Vestuario("Camiseta", 19.99, "Camiseta casual", 50, "Nike", "M", "Branca", "Algodão");
-        Alimenticio alimenticio = new Alimenticio("Bolacha", 2.99, "Bolacha recheada", 100, "Nestlé", "2023-12-31", "Farinha, açúcar, recheio");
-        Bebida bebida = new Bebida("Cerveja", 3.99, "Cerveja Lager", 200, "AmBev", "2024-06-30", "Água, malte, lúpulo", true, 355.0);
+        Scanner scanner = new Scanner(System.in);
 
-        
-        System.out.println("Produto Eletrônico:");
+        System.out.println("Cadastro de Produto Eletrônico:");
+        System.out.print("Nome: ");
+        String nomeEletronico = scanner.nextLine();
+        System.out.print("Preço: ");
+        double precoEletronico = scanner.nextDouble();
+        scanner.nextLine();
+        System.out.print("Descrição: ");
+        String descricaoEletronico = scanner.nextLine();
+        System.out.print("Modelo: ");
+        String modeloEletronico = scanner.nextLine();
+        System.out.print("Estoque: ");
+        int estoqueEletronico = scanner.nextInt();
+        scanner.nextLine();
+        System.out.print("Fabricante: ");
+        String fabricanteEletronico = scanner.nextLine();
+
+        Eletronico eletronico = new Eletronico(nomeEletronico, precoEletronico, descricaoEletronico, estoqueEletronico, fabricanteEletronico, modeloEletronico);
+
+        System.out.println("\nCadastro de Produto de Vestuário:");
+        System.out.print("Nome: ");
+        String nomeVestuario = scanner.nextLine();
+        System.out.print("Preço: ");
+        double precoVestuario = scanner.nextDouble();
+        scanner.nextLine();
+        System.out.print("Descrição: ");
+        String descricaoVestuario = scanner.nextLine();
+        System.out.print("Tamanho: ");
+        String tamanhoVestuario = scanner.nextLine();
+        System.out.print("Cor: ");
+        String corVestuario = scanner.nextLine();
+        System.out.print("Material: ");
+        String materialVestuario = scanner.nextLine();
+        System.out.print("Estoque: ");
+        int estoqueVestuario = scanner.nextInt();
+        scanner.nextLine();
+        System.out.print("Fabricante: ");
+        String fabricanteVestuario = scanner.nextLine();
+
+        Vestuario vestuario = new Vestuario(nomeVestuario, precoVestuario, descricaoVestuario, estoqueVestuario, fabricanteVestuario, tamanhoVestuario, corVestuario, materialVestuario);
+
+        System.out.println("\nCadastro de Produto Alimentício:");
+        System.out.print("Nome: ");
+        String nomeAlimenticio = scanner.nextLine();
+        System.out.print("Preço: ");
+        double precoAlimenticio = scanner.nextDouble();
+        scanner.nextLine();
+        System.out.print("Descrição: ");
+        String descricaoAlimenticio = scanner.nextLine();
+        System.out.print("Data de Validade: ");
+        String dataValidadeAlimenticio = scanner.nextLine();
+        System.out.print("Ingredientes: ");
+        String ingredientesAlimenticio = scanner.nextLine();
+        System.out.print("Estoque: ");
+        int estoqueAlimenticio = scanner.nextInt();
+        scanner.nextLine();
+        System.out.print("Fabricante: ");
+        String fabricanteAlimenticio = scanner.nextLine();
+
+        Alimenticio alimenticio = new Alimenticio(nomeAlimenticio, precoAlimenticio, descricaoAlimenticio, estoqueAlimenticio, fabricanteAlimenticio, dataValidadeAlimenticio, ingredientesAlimenticio);
+
+        System.out.println("\nCadastro de Bebida:");
+        System.out.print("Nome: ");
+        String nomeBebida = scanner.nextLine();
+        System.out.print("Preço: ");
+        double precoBebida = scanner.nextDouble();
+        scanner.nextLine();
+        System.out.print("Descrição: ");
+        String descricaoBebida = scanner.nextLine();
+        System.out.print("Data de Validade: ");
+        String dataValidadeBebida = scanner.nextLine();
+        System.out.print("Ingredientes: ");
+        String ingredientesBebida = scanner.nextLine();
+        System.out.print("Alcoólica (Sim/Não): ");
+        boolean alcoolicaBebida = scanner.nextLine().equalsIgnoreCase("Sim");
+        System.out.print("Volume (ml): ");
+        double volumeBebida = scanner.nextDouble();
+        scanner.nextLine();
+        System.out.print("Estoque: ");
+        int estoqueBebida = scanner.nextInt();
+        scanner.nextLine();
+        System.out.print("Fabricante: ");
+        String fabricanteBebida = scanner.nextLine();
+
+        Bebida bebida = new Bebida(nomeBebida, precoBebida, descricaoBebida, estoqueBebida, fabricanteBebida, dataValidadeBebida, ingredientesBebida, alcoolicaBebida, volumeBebida);
+
+    
+        System.out.println("\nProduto Eletrônico:");
         System.out.println("Nome: " + eletronico.getNome());
         System.out.println("Preço: $" + eletronico.getPreco());
         System.out.println("Descrição: " + eletronico.getDescricao());
@@ -45,6 +129,8 @@ public class TesteProdutos {
         System.out.println("Volume: " + bebida.getVolume() + " ml");
         System.out.println("Estoque: " + bebida.getEstoque());
         System.out.println("Fabricante: " + bebida.getFabricante());
+
+        scanner.close();
     }
 }
 
